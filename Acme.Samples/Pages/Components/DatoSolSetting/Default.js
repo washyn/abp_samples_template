cfa4217a2f79742c2e3de50f280cde241e8e8092
Washington Acero M <shin321654987@gmail.com>
2023-01-25 15:37:19 -0800

    improved with password visivility changer

--- Acme.Samples/Pages/Components/DatoSolSetting/Default.js
+++ Acme.Samples/Pages/Components/DatoSolSetting/Default.js
@@ -15,5 +15,28 @@
                 });
             abp.log.debug(formData)
         });
+
+        $("#Password").attr("type", "password");
+        
+        $("#PasswordVisibilityButton").click(function (e) {
+            let button = $(this);
+            let passwordInput = button.parent().find("input");
+            if (!passwordInput) {
+                return;
+            }
+
+            if (passwordInput.attr("type") === "password") {
+                passwordInput.attr("type", "text");
+            }
+            else {
+                passwordInput.attr("type", "password");
+            }
+
+            let icon = button.find("i");
+            if (icon) {
+                icon.toggleClass("fa-eye-slash").toggleClass("fa-eye");
+            }
+        });
+        
     });
 })(jQuery);
