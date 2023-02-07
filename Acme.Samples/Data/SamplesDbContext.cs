22f29d88ba2f94648e4abc9d15ca5fa1ccb5ce8c
Washington Acero M <shin321654987@gmail.com>
2023-02-06 18:50:25 -0800

    fixed for run proyect

--- Acme.Samples/Data/SamplesDbContext.cs
+++ Acme.Samples/Data/SamplesDbContext.cs
@@ -3,7 +3,7 @@ using Volo.Abp.AuditLogging.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.FeatureManagement.EntityFrameworkCore;
 using Volo.Abp.Identity.EntityFrameworkCore;
-using Volo.Abp.OpenIddict.EntityFrameworkCore;
+using Volo.Abp.IdentityServer.EntityFrameworkCore;
 using Volo.Abp.PermissionManagement.EntityFrameworkCore;
 using Volo.Abp.SettingManagement.EntityFrameworkCore;
 using Volo.Abp.TenantManagement.EntityFrameworkCore;
@@ -27,7 +27,7 @@ public class SamplesDbContext : AbpDbContext<SamplesDbContext>
         builder.ConfigureSettingManagement();
         builder.ConfigureAuditLogging();
         builder.ConfigureIdentity();
-        builder.ConfigureOpenIddict();
+        builder.ConfigureIdentityServer();
         builder.ConfigureFeatureManagement();
         builder.ConfigureTenantManagement();
 
