15444f5b68f77e617a37d07c822a0c431553c297
Washington Acero M <shin321654987@gmail.com>
2023-02-19 12:32:26 -0800

    aded sample files

---
+++ Acme.Samples/SunatCatalogModule.cs
@@ -0,0 +1,16 @@
+﻿using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Acme.Samples;
+
+[DependsOn(typeof(AbpVirtualFileSystemModule))]
+public class SunatCatalogModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            
+        });
+    }
+}
