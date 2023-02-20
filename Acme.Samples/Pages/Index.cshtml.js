1f29c6f18df832520a0b5e8eacdded0d383e9f1d
Washington Acero M <shin321654987@gmail.com>
2023-02-19 16:07:40 -0800

    aded sample of js for use component

--- Acme.Samples/Pages/Index.cshtml.js
+++ Acme.Samples/Pages/Index.cshtml.js
@@ -9,3 +9,41 @@
         modal.open();
     });
 })();
+
+
+
+$(function () {
+    // var l = abp.localization.getResource("CmsKit");
+
+    // var createModal = new abp.ModalManager(abp.appPath + "CmsKit/Tags/CreateModal");
+    // var updateModal = new abp.ModalManager(abp.appPath + "CmsKit/Tags/EditModal");
+
+    // var service = volo.cmsKit.admin.tags.tagAdmin;
+
+    // var getFilter = function () {
+    //     return {
+    //         filter: $('#CmsKitTagsWrapper input.page-search-filter-text').val()
+    //     };
+    // };
+    // abp.libs.datatables.createAjax(service.getList, getFilter);
+
+
+
+    // $('#CmsKitTagsWrapper form.page-search-form').on('submit', function (e) {
+    //     e.preventDefault();
+    //     dataTable.ajax.reload();
+    // });
+
+    // $('#AbpContentToolbar button[name=NewButton]').on('click', function (e) {
+    //     e.preventDefault();
+    //     createModal.open();
+    // });
+
+    // createModal.onResult(function () {
+    //     dataTable.ajax.reload();
+    // });
+
+    // updateModal.onResult(function () {
+    //     dataTable.ajax.reload();
+    // });
+});
