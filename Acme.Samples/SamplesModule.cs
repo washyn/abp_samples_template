bf5056e4408c4e0201dffcf17a184bf230c3e52f
Washington Acero M <shin321654987@gmail.com>
2023-03-11 13:57:53 -0800

    aded samples page of components

--- Acme.Samples/SamplesModule.cs
+++ Acme.Samples/SamplesModule.cs
@@ -1,10 +1,12 @@
-﻿using Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic;
+﻿using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Demo;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic;
 using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
 using Washyn.SbTheme;
 
 namespace Acme.Samples;
 
+[DependsOn(typeof(AbpAspNetCoreMvcUiBootstrapDemoModule))]
 [DependsOn(typeof(TemplateModules))]
 [DependsOn(typeof(AbpAutofacModule))]
 [DependsOn(typeof(WashynSbThemeModule))]
---
+++ Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Demo/AbpAspNetCoreMvcUiBootstrapDemoModule.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+// using Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.Demo;
+
+[DependsOn(
+    typeof(AbpAspNetCoreMvcUiThemeSharedModule),
+    typeof(AbpAutofacModule)
+    )]
+public class AbpAspNetCoreMvcUiBootstrapDemoModule : AbpModule
+{
+    // public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    // {
+    //     var app = context.GetApplicationBuilder();
+    //     var env = context.GetEnvironment();
+    //
+    //     if (env.IsDevelopment())
+    //     {
+    //         app.UseDeveloperExceptionPage();
+    //     }
+    //
+    //     app.UseRouting();
+    //     app.UseStaticFiles();
+    //     app.UseConfiguredEndpoints();
+    // }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<AbpAspNetCoreMvcUiBootstrapDemoModule>();
+            // if (hostingEnvironment.IsDevelopment())
+            // {
+            //     /* Using physical files in development, so we don't need to recompile on changes */
+            //     options.FileSets.ReplaceEmbeddedByPhysical<TemplateModules>(hostingEnvironment.ContentRootPath);
+            // }
+        });
+    }
+}
