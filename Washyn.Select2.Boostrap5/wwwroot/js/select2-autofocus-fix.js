f64a95b447b72e57c4bdf99c84f33912386e78d2
Washington Acero M <shin321654987@gmail.com>
2023-04-05 12:13:51 -0700

    aded comment

--- Washyn.Select2.Boostrap5/wwwroot/js/select2-autofocus-fix.js
+++ Washyn.Select2.Boostrap5/wwwroot/js/select2-autofocus-fix.js
@@ -1,4 +1,5 @@
-﻿$(function () {
+﻿// https://stackoverflow.com/questions/25882999/set-focus-to-search-text-field-when-we-click-on-select-2-drop-down
+$(function () {
     $(document).on('select2:open', () => {
         setTimeout(()=>{
             document.querySelector('.select2-search__field').focus();
