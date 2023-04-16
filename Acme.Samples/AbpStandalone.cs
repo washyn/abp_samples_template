f65444b3d3beb276fe25abfdde21987cacc2b7d6
Washington Acero M <shin321654987@gmail.com>
2023-04-15 21:27:59 -0700

    aded standalome modules

---
+++ Acme.Samples/AbpStandalone.cs
@@ -0,0 +1,175 @@
+//using Acme.Identity;
+using Acme.Samples.Data;
+using Microsoft.OpenApi.Models;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
+using Volo.Abp.AspNetCore.Serilog;
+using Volo.Abp.Autofac;
+using Volo.Abp.AutoMapper;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore.Sqlite;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.Swashbuckle;
+using Volo.Abp.UI.Navigation.Urls;
+using Volo.Abp.Uow;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Acme.Samples;
+
+[DependsOn(
+    // ABP Framework packages
+    typeof(AbpAspNetCoreMvcModule),
+    typeof(AbpAutofacModule),
+    typeof(AbpAutoMapperModule),
+    typeof(AbpEntityFrameworkCoreSqliteModule),
+    typeof(AbpSwashbuckleModule),
+    typeof(AbpAspNetCoreSerilogModule),
+    typeof(AbpAspNetCoreMvcUiBasicThemeModule)
+)]
+//[DependsOn(typeof(IdentityModule))]
+public class AbpStandalone : AbpModule
+{
+    public const bool IsMultiTenant = false;
+    
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+        var configuration = context.Services.GetConfiguration();
+        
+        ConfigureMultiTenancy();
+        ConfigureUrls(configuration);
+        ConfigureAutoMapper(context);
+        ConfigureSwagger(context.Services);
+        ConfigureAutoApiControllers();
+        ConfigureVirtualFiles(hostingEnvironment);
+        ConfigureLocalization();
+        ConfigureEfCore(context);
+    }
+    
+    private void ConfigureMultiTenancy()
+    {
+        Configure<AbpMultiTenancyOptions>(options =>
+        {
+            options.IsEnabled = IsMultiTenant;
+        });
+    }
+    
+    private void ConfigureUrls(IConfiguration configuration)
+    {
+        Configure<AppUrlOptions>(options =>
+        {
+            options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
+        });
+    }
+    
+    private void ConfigureLocalization()
+    {
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Languages.Add(new LanguageInfo("es", "es", "Español"));
+        });
+    }
+
+    private void ConfigureVirtualFiles(IWebHostEnvironment hostingEnvironment)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<TemplateModules>("Acme.Samples");
+        });
+    }
+
+    private void ConfigureAutoApiControllers()
+    {
+        Configure<AbpAspNetCoreMvcOptions>(options =>
+        {
+            options.ConventionalControllers.Create(typeof(TemplateModules).Assembly);
+        });
+    }
+
+    private void ConfigureSwagger(IServiceCollection services)
+    {
+        services.AddAbpSwaggerGen(
+            options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Samples API", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
+                options.CustomSchemaIds(type => type.FullName);
+            }
+        );
+    }
+
+    private void ConfigureAutoMapper(ServiceConfigurationContext context)
+    {
+        context.Services.AddAutoMapperObjectMapper<TemplateModules>();
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            options.AddMaps<TemplateModules>();
+        });
+    }
+
+    private void ConfigureEfCore(ServiceConfigurationContext context)
+    {
+        context.Services.AddAbpDbContext<SamplesDbContext>(options =>
+        {
+            options.AddDefaultRepositories(includeAllEntities: true);
+        });
+
+        Configure<AbpDbContextOptions>(options =>
+        {
+            options.Configure(configurationContext =>
+            {
+                configurationContext.UseSqlite();
+            });
+        });
+
+        Configure<AbpUnitOfWorkDefaultOptions>(options =>
+        {
+            options.TransactionBehavior = UnitOfWorkTransactionBehavior.Disabled;
+        });
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseAbpRequestLocalization();
+
+        if (!env.IsDevelopment())
+        {
+            app.UseErrorPage();
+        }
+
+        app.UseCorrelationId();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+
+        if (IsMultiTenant)
+        {
+            app.UseMultiTenancy();
+        }
+
+        app.UseUnitOfWork();
+        app.UseAuthorization();
+
+        app.UseSwagger();
+        app.UseAbpSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Samples API");
+        });
+
+        app.UseAuditing();
+        app.UseAbpSerilogEnrichers();
+        app.UseConfiguredEndpoints();
+    }
+}
