ad08e66450b4765a6cbdbc5b8da3d0880f0ae3a6
Washington Acero M <shin321654987@gmail.com>
2023-04-26 19:42:33 -0700

    aded refacts

---
+++ Washyn.ComprobantePdf/ComprobanteModule.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.CodeAnalysis;
+using Volo.Abp.Modularity;
+using Volo.Abp.TextTemplating;
+using Volo.Abp.TextTemplating.Razor;
+using Volo.Abp.UI.Navigation;
+using Volo.Abp.VirtualFileSystem;
+using WkHtmlToPdfDotNet;
+using WkHtmlToPdfDotNet.Contracts;
+
+namespace Washyn.ComprobantePdf;
+
+[DependsOn(typeof(AbpTextTemplatingRazorModule))]
+[DependsOn(typeof(AbpUiNavigationModule))]
+public class ComprobantePdfModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpRazorTemplateCSharpCompilerOptions>(options =>
+        {
+            options.References.Add(MetadataReference.CreateFromFile(typeof(ComprobantePdfModule).Assembly.Location));
+        });
+        
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<ComprobantePdfModule>("Washyn.ComprobantePdf");
+        });
+        
+        Configure<AbpNavigationOptions>(options =>
+        {
+            options.MenuContributors.Add(new ComprobantePdfMenuContributor());
+        });
+        
+        context.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+    }
+}
+
+public class ComprobantePdfMenuContributor : IMenuContributor
+{
+    public async Task ConfigureMenuAsync(MenuConfigurationContext context)
+    {
+        if (context.Menu.Name == StandardMenus.Main)
+        {
+            await ConfigureMainMenuAsync(context);
+        }
+    }
+
+    private async Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+    {
+        context.Menu.Items.Insert(
+            0,
+            new ApplicationMenuItem(
+                "Washyn.ComprobantePdf",
+                "Comprobante Pdf",
+                "~/ComprobantePdfSample"
+            )
+        );
+    }
+}
+
+public static class PdfConfig
+{
+    public static HtmlToPdfDocument GetDefaut(string htmlContent)
+    {
+        var globalSettings = new GlobalSettings()
+        {
+            ColorMode = ColorMode.Color,
+            Orientation = Orientation.Portrait,
+            PaperSize = PaperKind.A4,
+            Margins = new MarginSettings { Top = 5, Bottom = 10, Left = 5, Right = 5 }
+        };
+        var objSettings = new ObjectSettings()
+        {
+            PagesCount = true,
+            HtmlContent = htmlContent,
+            WebSettings = new WebSettings()
+            {
+                DefaultEncoding = System.Text.Encoding.UTF8.BodyName,
+                // UserStyleSheet = cssPath // styles not works
+            },
+            // Page = "http://google.com/"
+            // HeaderSettings = {FontSize = 9, Right = "Pagina [page] de [toPage]", Line = true, Spacing = 2.812}
+        };
+        
+        var doc = new HtmlToPdfDocument()
+        {
+            GlobalSettings = globalSettings,
+            Objects = { objSettings}
+        };
+
+        return doc;
+    }
+}
---
+++ Washyn.ComprobantePdf/PdfTemplateDefinitionProvider.cs
@@ -0,0 +1,33 @@
+﻿using Volo.Abp.TextTemplating;
+using Volo.Abp.TextTemplating.Razor;
+
+namespace Washyn.ComprobantePdf;
+
+public class PdfTemplateDefinitionProvider : TemplateDefinitionProvider
+{
+    public override void Define(ITemplateDefinitionContext context)
+    {
+        context.Add(
+            new TemplateDefinition("Layout", isLayout: true)
+                .WithRazorEngine()
+                .WithVirtualFilePath(
+                    "/Templates/Layout.cshtml",
+                    isInlineLocalized: true
+                )
+                .WithRazorEngine()
+        );
+        
+        context.Add(
+            new TemplateDefinition(
+                    name: "Comprobante",
+                    layout: "Layout" 
+                )
+                .WithRazorEngine()
+                .WithVirtualFilePath(
+                    "/Templates/Comprobante.cshtml",
+                    isInlineLocalized: true
+                )
+        );
+        
+    }
+}
